948caf66907f31d0a51d98aae8135ef0627fad49
Soykat Amin <soykatamin@gmail.com>
2024-06-25 19:49:14 +0200

    Added buffer sizes, storage and indexes statistics. Clear Statistics done.

--- stats.c
+++ stats.c
@@ -1,2 +1,9 @@
 #include "stats.h"
 
+void clear_statistics(void) {
+    for (uint8_t i = 0; i < HOUR_STATS; i++) hourStats[i] = 0;
+    for (uint8_t i = 0; i < DAY_STATS; i++) dayStats[i] = 0;
+    for (uint8_t i = 0; i < MONTH_STATS; i++) monthStats[i] = 0;
+    for (uint8_t i = 0; i < YEAR_STATS; i++) yearStats[i] = 0;
+    minuteIndex = hourIndex = dayIndex = monthIndex = 0;
+}
--- stats.h
+++ stats.h
@@ -1,4 +1,23 @@
 #pragma once
+#include <avr/io.h>
+
+// Buffer sizes for statistics
+const uint8_t hour_stats = 60;
+const uint8_t day_stats = 24;
+const uint8_t month_stats = 30;
+const uint8_t year_stats = 12;
+
+// Storage for statistics
+float hourStats[hour_stats];
+float dayStats[day_stats];
+float monthStats[month_stats];
+float yearStats[year_stats];
+
+// Indexes for statistics
+uint8_t minuteIndex = 0;
+uint8_t hourIndex = 0;
+uint8_t dayIndex = 0;
+uint8_t monthIndex = 0;
 
 void clear_statistics(void);
 
