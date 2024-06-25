83118a76339f659e6db27ccb86420eb20255c682
Soykat Amin <soykatamin@gmail.com>
2024-06-25 17:29:17 +0200

    Added stats prototypes. Implemented a simple interface in main.

--- main.c
+++ main.c
@@ -1,5 +1,27 @@
 #pragma once
+#include "stats.h"
+#include "./my_uart/my_uart.h"
 
 int main(){
+
+    char command;
+    while (1) {
+        switch (command) {
+            case 'o':
+                // Set to online mode
+                break;
+            case 'c':
+                clear_statistics();
+                UART_putString("Statistics Cleared\n");
+                break;
+            case 'q':
+                query_statistics();
+                break;
+            default:
+                UART_putString("Unknown Command\n");
+                break;
+        }
+    }
+
     return 0;
 }
---
+++ stats.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void clear_statistics(void);
+
+void query_statistics(void);
